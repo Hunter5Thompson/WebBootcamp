8749c43193eaa8a5a7143c581a7ee82c6d83fbfc
strosche <robert.scheike@student.hs-anhalt.de>
2021-04-14 15:03:06 +0200

    Added logic to the Next7Days Component it is now possible to add multiple ToDosand there will be an response / visual feedback in the app and also the data is written on the Server.

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import moment from 'moment';
+import React, {useState, useEffect} from 'react';
+import ToDo from './ToDo';
 
-function Next7Days() {
+function Next7Days({toDos}) {
+  const [weekToDos, setWeekToDos] = useState([])
 
-    return(
-        <div className="Next7Days">
-            User---------------Befülle er mich--------------User
-            </div>
+  useEffect(() =>{
+      const days = ['0' , '1' , '2' , '3' , '4', '5' , '6']
 
-    )
+
+      const sortedToDosByDays = days.map( day => {
+          return {
+              toDos : toDos.filter( todo => todo.day === day ),
+              number : day
+          }
+      })  
+
+      const today = parseInt(moment().format('d'))
+
+      const arrangeDays = sortedToDosByDays.slice(today).concat(sortedToDosByDays.slice('0',today))
+
+      setWeekToDos(arrangeDays)
+  }, [toDos]);
+
+
+
+
+
+
+return (
+    <div className="Next7Days">
+        {
+
+            weekToDos.map( day => 
+                <div key={day.number}>
+                    <div className="day">
+                        <div className="name">
+                            {moment(day.number, 'd').format('dddd')}
+                            { day.number === moment().format('d') && '(Heute)'}
+                        </div>
+                        <div className="total-toDos">
+                            ({day.toDos.length})
+                        </div>
+                        </div>
+                        <div className="todos">
+                            {
+                                day.toDos.map( todo =>
+                                    <ToDo key={todo.id} todo={todo}/>
+                                    )
+                            }
+                        </div>
+                    </div>
+
+                )
+        }
+        </div>
+
+)
 }
-
 export default Next7Days;
