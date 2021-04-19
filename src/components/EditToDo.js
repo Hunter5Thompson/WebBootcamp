79b391119f62291d24d0beb12128404797af4358
strosche <robert.scheike@student.hs-anhalt.de>
2021-04-19 22:17:56 +0200

    EditToDo is now fully operable it updates the ToDo component immidetly

--- src/components/EditToDo.js
+++ src/components/EditToDo.js
@@ -4,6 +4,7 @@ import React, {useState, useContext, useEffect} from 'react';
 import ToDoForm from './ToDoForm';
 import {ToDoContext} from '../context';
 import moment from 'moment';
+import firebase from '../firebase';
 
 
 function EditToDo() {
@@ -22,11 +23,28 @@ function EditToDo() {
         if(selectedToDo){
             setText(selectedToDo.text)
             setDay(moment(selectedToDo.date, 'DD/MM/YYYY'))
-            setTime(moment(selectedToDo.time, 'HH:mm'))
+            setTime(moment(selectedToDo.time, 'hh:mm A'))
             setToDoProject(selectedToDo.projectName)
         }
     }, [selectedToDo])
   
+
+    useEffect(() => {
+        if(selectedToDo){
+            firebase
+            .firestore()
+            .collection('ToDos')
+            .doc(selectedToDo.id)
+            .update({
+                text,
+                date : moment(day).format('DD/MM/YYYY'),
+                day : moment(day).format('d'),
+                time : moment(time).format('hh:mm A'),
+                projectName : selectedToDo
+            })
+        }
+    }, [text, day, time, toDoProject, selectedToDo])
+
 
     function handleSubmit() {
 
