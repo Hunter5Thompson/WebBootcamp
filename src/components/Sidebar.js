2c3a6890c1406d17de0d425a09af777065f19167
strosche <robert.scheike@student.hs-anhalt.de>
2021-04-19 21:46:58 +0200

    Sidebar is now able to show editrtodo component when clicked otherwise toogle to of

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useRef, useContext} from 'react';
+import {ToDoContext} from '../context'
 
 function Sidebar({children}) {
 
+    const {setSelectedToDo} = useContext(ToDoContext)
+    const sidebarRef = useRef();
+
+    useEffect(() => {
+        document.addEventListener('click', handleClick)
+
+        return () => document.removeEventListener('click', handleClick)
+    })
+
+    const handleClick = e => {
+        if(e.target === sidebarRef.current || sidebarRef.current.contains(e.target)){
+            setSelectedToDo(undefined)
+        }
+    }
+
+
     return(
-        <div className="Sidebar">
+        <div 
+            className="Sidebar"
+            ref={sidebarRef}
+        >
             {children}
             </div>
 
